485d1c15101285096debcef164962a4f36d981eb
Vinh Nguyen <vinhgrh3@gmail.com>
2023-06-20 17:49:43 +0700

    Test

---
+++ 06-06-main.js
@@ -0,0 +1,12 @@
+function isSymmetricNumber(n) {
+  if (n < 0 || n >= 1000) return false;
+  if (n < 10) return true;
+  if (n >= 10 && n <= 99) {
+    if (n % 10 === Math.trunc(n / 10)) return true;
+  }
+  return n % 10 === Math.trunc(n / 100);
+}
+console.log(isSymmetricNumber(10));
+console.log(isSymmetricNumber(10));
+console.log(isSymmetricNumber(10));
+console.log(isSymmetricNumber(10));
